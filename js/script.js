d6abf5b888b0e39520756e3b94c167d6f164fa02
abdulrahmanbhatti <abdulrehmanbhatti0010@gmail.com>
2026-06-05 12:34:51 +0500

    feature

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
 let search = document.querySelector(".search-form");
 document.querySelector("#search-btn").onclick= ()=>{
     search.classList.toggle("active");
+    cart.classList.remove("active");
+    login.classList.remove("active");
+    nav.classList.remove("active");
+}
+let cart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-btn").onclick=()=>{
+    cart.classList.toggle("active");
+    search.classList.remove("active");
+    login.classList.remove("active");
+    nav.classList.remove("active");
+}
+let login = document.querySelector(".login-form");
+document.querySelector("#login-btn").onclick=()=>{
+    login.classList.toggle("active");
+    search.classList.remove("active");
+    cart.classList.remove("active");
+    nav.classList.remove("active");
+}
+let nav = document.querySelector(".navbar");
+document.querySelector("#menu-btn").onclick=()=>{
+    nav.classList.toggle("active");
+    search.classList.remove("active");
+    cart.classList.remove("active");
+    login.classList.remove("active");
+   
+}
+
+
+window.onscroll=()=>{
+    search.classList.remove("active");
+    cart.classList.remove("active");
+    login.classList.remove("active");
+    nav.classList.remove("active");
+     
 }
